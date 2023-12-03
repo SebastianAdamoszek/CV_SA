4617ec4685233d43e78f94ab191f8def045d8b27
SebastianAdamoszek <sebaadamoszek@vp.pl>
2023-12-04 00:11:07 +0100

    link url, image

--- js/gallery.js
+++ js/gallery.js
@@ -1,67 +1,69 @@
-  // link local repozytory  
-    // "../img/gallery/001.jpg",
-    // "../img/gallery/002.jpg",
-    // "../img/gallery/003.jpg",
-    // "../img/gallery/004.jpg",
-    // "../img/gallery/005.jpg",
-    // "../img/gallery/006.jpg",
+// link local repozytory
+// "../img/gallery/001.jpg",
+// "../img/gallery/002.jpg",
+// "../img/gallery/003.jpg",
+// "../img/gallery/004.jpg",
+// "../img/gallery/005.jpg",
+// "../img/gallery/006.jpg",
 
 const images = [
-    "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/001.jpg?raw=true"
-  
+  "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/001.jpg?raw=true",
+  "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/002.jpg?raw=true",
+  "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/003.jpg?raw=true",
+  "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/004.jpg?raw=true",
+  "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/005.jpg?raw=true",
+  "https://github.com/SebastianAdamoszek/CV/blob/main/img/gallery/006.jpg?raw=true",
+];
+let currentIndex = 0;
 
-  ];
-  let currentIndex = 0;
-  
-  const backdrop = document.querySelector(".backdrop");
-  const modal = document.querySelector(".modal");
-  const prevButton = document.getElementById("prevButton");
-  const nextButton = document.getElementById("nextButton");
-  const imageElement = document.getElementById("image");
-  
-  function openModal() {
-    modal.classList.add("is-visible");
-    backdrop.classList.add("is-visible");
-  
-    showCurrentImage();
-    window.addEventListener("keydown", escapeKeyHandler);
-    backdrop.addEventListener("click", backdropClickHandler);
-    prevButton.addEventListener("click", prevImage);
-    nextButton.addEventListener("click", nextImage);
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+const imageElement = document.getElementById("image");
+
+function openModal() {
+  modal.classList.add("is-visible");
+  backdrop.classList.add("is-visible");
+
+  showCurrentImage();
+  window.addEventListener("keydown", escapeKeyHandler);
+  backdrop.addEventListener("click", backdropClickHandler);
+  prevButton.addEventListener("click", prevImage);
+  nextButton.addEventListener("click", nextImage);
+}
+
+function closeModal() {
+  modal.classList.remove("is-visible");
+  backdrop.classList.remove("is-visible");
+  window.removeEventListener("keydown", escapeKeyHandler);
+  backdrop.removeEventListener("click", backdropClickHandler);
+  prevButton.removeEventListener("click", prevImage);
+  nextButton.removeEventListener("click", nextImage);
+}
+
+function escapeKeyHandler(event) {
+  if (event.key === "Escape") {
+    closeModal();
   }
-  
-  function closeModal() {
-    modal.classList.remove("is-visible");
-    backdrop.classList.remove("is-visible");
-    window.removeEventListener("keydown", escapeKeyHandler);
-    backdrop.removeEventListener("click", backdropClickHandler);
-    prevButton.removeEventListener("click", prevImage);
-    nextButton.removeEventListener("click", nextImage);
+}
+
+function backdropClickHandler(event) {
+  if (event.target.classList.contains("backdrop")) {
+    closeModal();
   }
-  
-  function escapeKeyHandler(event) {
-    if (event.key === "Escape") {
-      closeModal();
-    }
-  }
-  
-  function backdropClickHandler(event) {
-    if (event.target.classList.contains("backdrop")) {
-      closeModal();
-    }
-  }
-  
-  function showCurrentImage() {
-    imageElement.src = images[currentIndex];
-  }
-  
-  function prevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showCurrentImage();
-  }
-  
-  function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showCurrentImage();
-  }
- 
+}
+
+function showCurrentImage() {
+  imageElement.src = images[currentIndex];
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showCurrentImage();
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showCurrentImage();
+}
