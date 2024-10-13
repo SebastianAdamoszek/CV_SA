d79d4a80e9bd46c9e35ba930698ca74b41f278b7
SebastianAdamoszek <127986852+SebastianAdamoszek@users.noreply.github.com>
2024-10-13 04:13:39 +0000

    Deploying to gh-pages from @ SebastianAdamoszek/CV_DE@fadd7095ed9769ed6639c00b9fc101edfba8d6f6 🚀

---
+++ index.026e6047.js
@@ -0,0 +1,2 @@
+const myButton=document.querySelector(".button-up");function scrollFunction(){document.body.scrollTop>200||document.documentElement.scrollTop>200?myButton.style.display="block":myButton.style.display="none"}function topFunction(){window.scrollTo({top:0,behavior:"smooth"})}myButton.addEventListener("click",topFunction);
+//# sourceMappingURL=index.026e6047.js.map
